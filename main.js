56a38344b5b9dd781f43e7c79de1ca8b98251674
Alishba Wajahat <alishbawajahat867@gmail.com>
2024-02-29 00:56:21 +0500

    Added two files

---
+++ main.js
@@ -0,0 +1,16 @@
+//Example of primitive data types
+// Example of string
+var Name = "Aliishba";
+console.log(Name);
+// Example of age
+var Age = 23;
+console.log(Age);
+// Example of boolean
+var Interest = true;
+console.log(Interest);
+// Example of null
+var Assets = null;
+console.log(Assets);
+// Example of undefined
+var Qualification = undefined;
+console.log(Qualification);
---
+++ main.ts
@@ -0,0 +1,25 @@
+//Example of primitive data types
+// Example of string
+var Name:string="Aliishba";
+console.log(Name);
+
+// Example of age
+var Age:number=23;
+console.log(Age);
+
+// Example of boolean
+var Interest:boolean=true;
+console.log(Interest);
+
+// Example of null
+var Assets:null=null;
+console.log(Assets);
+
+// Example of undefined
+var Qualification:undefined=undefined;
+console.log(Qualification);
+
+
+
+
+
